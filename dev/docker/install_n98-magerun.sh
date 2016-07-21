99cb229e92483bf6556aceb0c6d38c5da670d4d3
Sándor Czettner <sandor@healthywebsites.co.uk>
2016-07-21 14:56:34 +0100

    www-data user now will receive the same uid as the /vagrant folder

--- dev/docker/install_n98-magerun.sh
+++ dev/docker/install_n98-magerun.sh
@@ -7,11 +7,11 @@ fi
 
 echo "=> Instaling n98-magerun"
 cd /tmp
-/usr/bin/wget https://raw.githubusercontent.com/netz98/n98-magerun/master/n98-magerun.phar
+wget https://files.magerun.net/n98-magerun.phar
 /bin/chmod +x ./n98-magerun.phar
 cp ./n98-magerun.phar /usr/local/bin/n98-magerun
 
-/usr/bin/wget https://raw.githubusercontent.com/netz98/n98-magerun2/master/n98-magerun2.phar
+/usr/bin/wget https://files.magerun.net/n98-magerun2.phar
 /bin/chmod +x ./n98-magerun2.phar
 cp ./n98-magerun2.phar /usr/local/bin/n98-magerun2
 
--- dev/docker/run.sh
+++ dev/docker/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+usermod -u $(ls -ldn /vagrant | awk '{print $3}') www-data
+groupmod -g $(ls -ldn /vagrant | awk '{print $3}') www-data
+
 exec supervisord -n
